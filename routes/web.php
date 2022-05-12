6dd8c847dc6b2683f67096a6d74baae2615b1d87
Tihon Pavlov <f1ssur4@Tihons-Air.N4>
2022-05-12 21:09:07 +0300

    fix last commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/tasks', function () {
+    return 'tasks';
+});
+
+Route::get('/lessons', function () {
+    return 'lessons';
+});
+
+Route::get('/clients', function () {
+    return 'clients-list';
+});
+
+Route::get('/teachers', function () {
+    return 'teachers-list';
+});
+
+Route::get('/arts', function () {
+    return 'arts';
+});
+
+Route::get('/statistics', function () {
+    return 'statistics';
+});
